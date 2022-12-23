d63f67ad63470c84a78c744f568cbbe00647163d
RafaelFacundo <rafaelfacundo63@gmail.com>
2022-12-23 08:30:55 -0300

    BinaryTree finished

--- data_structures/code_in_C/BinaryTree.c
+++ data_structures/code_in_C/BinaryTree.c
@@ -12,26 +12,23 @@ typedef struct root_ {
     node *rootOfTree;
 } root;
 
-root* createBinaryTree() {
+root* createBinaryTree(int valueToInsert) {
     root* rootOfTree = malloc(sizeof(root));
     node* firstNodeOfTree = malloc(sizeof(node));
     firstNodeOfTree->left = NULL;
     firstNodeOfTree->right = NULL;
     firstNodeOfTree->father = NULL;
     rootOfTree->rootOfTree = firstNodeOfTree;
-    
-    printf("Enter the root value: ");
-    scanf("%d", &rootOfTree->rootOfTree->value);
+    rootOfTree->rootOfTree->value = valueToInsert;
     return rootOfTree;
 }
 
-node* createNodeToInsertInTree() {
+node* createNodeToInsertInTree(int value) {
     node* newNode = malloc(sizeof(node));
     newNode->left = NULL;
     newNode->right = NULL;
     newNode->father = NULL;
-    printf("Enter the node value: ");
-    scanf("%d", &newNode->value);
+    newNode->value = value;
     return newNode;
 }
 
@@ -56,9 +53,64 @@ void runThroughTreeToInsertNode(root* Tree, node* newNode) {
     }
 }
 
-void addNode(root *rootOfTree) {
-    node* newNodeToInsert = createNodeToInsertInTree();
+void addNode(root *rootOfTree, int value) {
+    node* newNodeToInsert = createNodeToInsertInTree(value);
     runThroughTreeToInsertNode(rootOfTree, newNodeToInsert);
+}
+
+node *minimum(node *rootOfSomeTree) {
+    node *nodeToIterate = rootOfSomeTree;
+    while(nodeToIterate->left != NULL) {
+        nodeToIterate = nodeToIterate->left;
+    };
+    return nodeToIterate;
+
+}
+
+node *maximum(node *rootOfSomeTree) {
+    node *nodeToIterate = rootOfSomeTree;
+    while(nodeToIterate->right != NULL) {
+        nodeToIterate = nodeToIterate->right;
+    };
+    return nodeToIterate;
+}
+
+void transplant(root *Tree, node *nodeToBeRemoved, node *nodeToBeMoved) {
+    if (nodeToBeRemoved->father == NULL) {
+        Tree->rootOfTree = nodeToBeMoved;
+    }else if (nodeToBeRemoved == nodeToBeRemoved->father->left) {
+        nodeToBeRemoved->father->left = nodeToBeMoved;
+    }else {
+        nodeToBeRemoved->father->right = nodeToBeMoved;
+    }
+
+    if (nodeToBeMoved != NULL) {
+        nodeToBeMoved->father = nodeToBeRemoved->father;
+    }
+
+    //free(nodeToBeRemoved);
+}
+
+void removeNodeFromTree(root *Tree, node *nodeToBeRemoved) {
+    if ( nodeToBeRemoved->left == NULL ) {
+        transplant(Tree, nodeToBeRemoved, nodeToBeRemoved->right);
+    }else if ( nodeToBeRemoved->right == NULL ) {
+        transplant(Tree, nodeToBeRemoved, nodeToBeRemoved->left);
+    }else {
+        //getting the successor of the node to be removed
+        node *y = minimum(nodeToBeRemoved);
+        transplant(Tree, y, y->right);
+        y->left = nodeToBeRemoved->left;
+        nodeToBeRemoved->left->father = y;
+        y->right = nodeToBeRemoved->right;
+        if (nodeToBeRemoved->right != NULL) {
+            nodeToBeRemoved->right->father = y;
+        }
+        transplant(Tree, nodeToBeRemoved, y);
+    }
+
+    nodeToBeRemoved = NULL;
+    free(nodeToBeRemoved);
 }
 
 void printTreeInOrder(node* nodeToVisit) {
@@ -69,14 +121,40 @@ void printTreeInOrder(node* nodeToVisit) {
     }
 }
 
+node *findAnode(root *Tree, int valueToSearch) {
+    node *nodeToIterate = Tree->rootOfTree;
+    
+    while (nodeToIterate != NULL && nodeToIterate->value != valueToSearch){
+        if (valueToSearch < nodeToIterate->value) {
+            nodeToIterate = nodeToIterate->left;
+        }else if (valueToSearch > nodeToIterate->value) {
+            nodeToIterate = nodeToIterate->right;
+        }
+    }
+    
+   /*  nodeToIterate = NULL;
+    free(nodeToIterate); */
+    return nodeToIterate;
+    
+}
+
+void printInOrder(node* nodeToVisit) {
+    printTreeInOrder(nodeToVisit);
+    printf("\n");
+}
+
 
 int main() {
-    root* rootTree = createBinaryTree();
-    int n = 0;addNode(rootTree);
+    root* rootTree = createBinaryTree(5);
+    int n = 0;
     while(n < 7) {
-        addNode(rootTree);
+        addNode(rootTree, n);
         n += 1;
     }
-    printTreeInOrder(rootTree->rootOfTree);
+     printInOrder(rootTree->rootOfTree);
+    node *nodeToRemove = findAnode(rootTree, 4);
+    printf("valor do no: %d\n", nodeToRemove->value);
+    removeNodeFromTree(rootTree, nodeToRemove); 
+    printInOrder(rootTree->rootOfTree); 
     return 0;
 }
